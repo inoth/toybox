a97c1021c4f074246b873602b4748f49f2b564f0
inoth <inoth.work@gmail.com>
2022-09-13 17:58:39 +0800

    .

--- util/util.go
+++ util/util.go
@@ -6,23 +6,23 @@ import (
 
 const SIGNKEY = "BA5ktbKaV47uOcQpnuUT76GvBRYpMdHX"
 
-// func Max[T int | int32 | int64 | float32 | float64 | uint | uint32 | uint64](a, b T) T {
-// 	if a > b {
-// 		return a
-// 	}
-// 	return b
-// }
+func Max[T int | int32 | int64 | float32 | float64 | uint | uint32 | uint64](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
 
-// func Min[T int | int32 | int64 | float32 | float64 | uint | uint32 | uint64](a, b T) T {
-// 	if a < b {
-// 		return a
-// 	}
-// 	return b
-// }
+func Min[T int | int32 | int64 | float32 | float64 | uint | uint32 | uint64](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
 
-// func Add[T int | int32 | int64 | float32 | float64 | uint | uint32 | uint64 | string](a, b T) T {
-// 	return a + b
-// }
+func Add[T int | int32 | int64 | float32 | float64 | uint | uint32 | uint64 | string](a, b T) T {
+	return a + b
+}
 
 func UUID(n ...int) string {
 	if len(n) > 0 {
