00cc0aefc127ee8774aa5b4fdc9f9856115662f8
inoth <inoth.work@gmail.com>
2024-01-29 18:07:05 +0800

    🐞 fix:

--- server/metric/metric.go
+++ server/metric/metric.go
@@ -1,90 +1,89 @@
 package metric
 
 import (
-	"errors"
-
-	"github.com/inoth/toybox/util"
+	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Metric struct {
-	Id        string
-	Name      string
-	Desc      string
-	Type      string
-	Args      []string
-	Collector prometheus.Collector
+	id string
+	// collector prometheus.Collector
+
+	Name string   `toml:"name"`
+	Desc string   `toml:"desc"`
+	Type string   `toml:"type"`
+	Args []string `toml:"args"`
 }
 
-func NewMetric(subsystem, name, namespace, desc, metricType string, args ...string) *Metric {
+type metrics struct {
+	metricType string
+	collector  prometheus.Collector
+}
+
+func (m *Metric) initMetric(subsystem, namespace string) *metrics {
 	var collector prometheus.Collector
-	switch metricType {
+	switch m.Type {
 	case Counter:
 		collector = prometheus.NewCounter(prometheus.CounterOpts{
 			Subsystem: subsystem,
 			Namespace: namespace,
-			Name:      name,
-			Help:      desc,
+			Name:      m.Name,
+			Help:      m.Desc,
 		})
 	case CounterVec:
 		collector = prometheus.NewCounterVec(prometheus.CounterOpts{
 			Subsystem: subsystem,
 			Namespace: namespace,
-			Name:      name,
-			Help:      desc,
-		}, args)
+			Name:      m.Name,
+			Help:      m.Desc,
+		}, m.Args)
 	case Gauge:
 		collector = prometheus.NewGauge(prometheus.GaugeOpts{
 			Subsystem: subsystem,
 			Namespace: namespace,
-			Name:      name,
-			Help:      desc,
+			Name:      m.Name,
+			Help:      m.Desc,
 		})
 	case GaugeVec:
 		collector = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 			Subsystem: subsystem,
 			Namespace: namespace,
-			Name:      name,
-			Help:      desc,
-		}, args)
+			Name:      m.Name,
+			Help:      m.Desc,
+		}, m.Args)
 	case Histogram:
 		collector = prometheus.NewHistogram(prometheus.HistogramOpts{
 			Subsystem: subsystem,
 			Namespace: namespace,
-			Name:      name,
-			Help:      desc,
+			Name:      m.Name,
+			Help:      m.Desc,
 		})
 	case HistogramVec:
 		collector = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 			Subsystem: subsystem,
 			Namespace: namespace,
-			Name:      name,
-			Help:      desc,
-		}, args)
+			Name:      m.Name,
+			Help:      m.Desc,
+		}, m.Args)
 	case Summary:
 		collector = prometheus.NewSummary(prometheus.SummaryOpts{
 			Subsystem: subsystem,
 			Namespace: namespace,
-			Name:      name,
-			Help:      desc,
+			Name:      m.Name,
+			Help:      m.Desc,
 		})
 	case SummaryVec:
 		collector = prometheus.NewSummaryVec(prometheus.SummaryOpts{
 			Subsystem: subsystem,
 			Namespace: namespace,
-			Name:      name,
-			Help:      desc,
-		}, args)
+			Name:      m.Name,
+			Help:      m.Desc,
+		}, m.Args)
 	default:
 		panic(errors.New("type of invalid indicator"))
 	}
-
-	return &Metric{
-		Id:        util.UUID16(),
-		Name:      name,
-		Desc:      desc,
-		Type:      metricType,
-		Args:      args,
-		Collector: collector,
+	return &metrics{
+		metricType: m.Type,
+		collector:  collector,
 	}
 }
--- server/metric/prometheus.go
+++ server/metric/prometheus.go
@@ -1,5 +1,14 @@
 package metric
 
+import (
+	"context"
+	"net/http"
+
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
 // https://github.com/zsais/go-gin-prometheus/blob/master/middleware.go
 
 var (
@@ -13,8 +22,56 @@ var (
 	SummaryVec   string = "summary_vec"
 )
 
-type Prometheus struct {
-	metric []*Metric
+var (
+	prom *Prometheus
+)
 
+type Prometheus struct {
+	ready     bool
+	Port      string `toml:"port"`
 	Subsystem string `toml:"subsystem"`
+	Namespace string `toml:"namespace"`
+
+	Metrics    []Metric
+	collectors map[string]*metrics
+}
+
+func (pm *Prometheus) IsReady() {
+	pm.ready = true
+}
+
+func (pm *Prometheus) Ready() bool {
+	return pm.ready
+}
+
+func (pm *Prometheus) Name() string {
+	return pm.Namespace
+}
+
+func (pm *Prometheus) register() error {
+	for _, item := range pm.Metrics {
+		col := item.initMetric(pm.Subsystem, pm.Namespace)
+		if _, ok := pm.collectors[item.Name]; !ok {
+			pm.collectors[item.Name] = col
+
+			if err := prometheus.Register(col.collector); err != nil {
+				return errors.Wrap(err, "register initMetric err")
+			}
+		}
+	}
+	return nil
+}
+
+func (pm *Prometheus) Run(ctx context.Context) error {
+
+	if err := pm.register(); err != nil {
+		return err
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	if err := http.ListenAndServe(pm.Port, nil); err != nil {
+		return errors.Wrap(err, "run ListenAndServe err")
+	}
+	return nil
 }
